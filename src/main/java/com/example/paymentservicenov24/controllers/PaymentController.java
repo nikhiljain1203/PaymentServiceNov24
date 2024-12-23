2721a264de659b266b2a5b68ab32d22ccd0e8cf7
apple <nikhiljain1203@gmail.com>
2024-12-23 22:53:42 +0530

    Second Class

--- src/main/java/com/example/paymentservicenov24/controllers/PaymentController.java
+++ src/main/java/com/example/paymentservicenov24/controllers/PaymentController.java
@@ -2,18 +2,19 @@ package com.example.paymentservicenov24.controllers;
 
 import com.example.paymentservicenov24.dtos.InitiatePayementDto;
 import com.example.paymentservicenov24.services.PaymentService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.paymentservicenov24.services.ProductService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/payments")
 public class PaymentController {
     private PaymentService paymentService;
+    private ProductService productService;
 
-    public PaymentController(PaymentService paymentService) {
+    public PaymentController(PaymentService paymentService,
+                             ProductService productService) {
         this.paymentService = paymentService;
+        this.productService = productService;
     }
 
     @PostMapping
@@ -30,5 +31,10 @@ public class PaymentController {
     public String listenToWebhook(@RequestBody String webhookEvent) {
         System.out.println(webhookEvent);
         return "OK";
+    }
+
+    @GetMapping("/product/{id}")
+    public String getProductDetails(@PathVariable String id) {
+        return productService.getProductDetails(id);
     }
 }
